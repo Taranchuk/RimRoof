434f5b688fa1a90b52d4d2b2e353fa38150f57bf
Taranchuk <nfhfyxer@gmail.com>
2026-09-11 17:03:41 +0300

    fixes

--- 1.6/Source/Graphic_Roof.cs
+++ 1.6/Source/Graphic_Roof.cs
@@ -20,12 +20,9 @@ namespace RimRoof
             subGraphics[3] = GraphicDatabase.Get<Graphic_Single>(req.path + "_inner_corner", req.shader, drawSize, color);
         }
 
-        public Vector3 RoofDrawOffset => new Vector3(0f, 0f, 0.2f);
-
         public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
         {
             if (thing is Building_Roof roof && !roof.ShouldBeVisible) return;
-            loc += RoofDrawOffset;
             var graphic = thing != null ? GetSub(thing) : subGraphics[0];
             graphic.DrawWorker(loc, thing != null ? thing.Rotation : rot, thingDef, thing, extraRotation);
         }
@@ -36,8 +33,7 @@ namespace RimRoof
             var graphic = GetSub(thing);
             var rot = thing.Rotation;
             var angle = rot.AsAngle + extraRotation;
-            var pos = thing.TrueCenter() + RoofDrawOffset;
-            Printer_Plane.PrintPlane(layer, pos, drawSize, graphic.MatAt(rot, thing), angle);
+            Printer_Plane.PrintPlane(layer, thing.TrueCenter(), drawSize, graphic.MatAt(rot, thing), angle);
         }
 
         private Graphic GetSub(Thing t) => (t is Building_Roof b) ? subGraphics[(int)b.CurShape] : subGraphics[0];
