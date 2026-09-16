3f4d51f39c2b4b6c0e792c47a76b502cee8b8bbf
Taranchuk <nfhfyxer@gmail.com>
2026-09-16 09:38:58 +0300

    textures incorporated

--- 1.6/Source/Startup.cs
+++ 1.6/Source/Startup.cs
@@ -6,7 +6,7 @@ namespace RimRoof
     [StaticConstructorOnStartup]
     public static class Startup
     {
-        public static readonly Texture2D IconLogFrame = ContentFinder<Texture2D>.Get("Log_Frame");
+        public static readonly Texture2D IconLogFrame = ContentFinder<Texture2D>.Get("Things/Building/Misc/Log_Frame");
         public static readonly Texture2D IconInnerRoof = ContentFinder<Texture2D>.Get("Roofs/Haygrass_Roof");
     }
 }
